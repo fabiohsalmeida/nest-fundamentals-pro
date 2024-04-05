a2f2785db13128dcc4d357437bf5cc05fe02d029
Fabio Almeida <fabio.h.s.almeida@gmail.com>
2024-04-05 20:26:11 -0300

    Including docker compose with postgres and postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,34 @@ import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [SongsModule],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        database: 'nest-spotify-like-project',
+        host: 'localhost',
+        port: 5432,
+        username: 'myuser',
+        password: 'secret',
+        entities: [],
+        // Never use in production it, it will erase part of database
+        synchronize: true
+      }
+    ),
+    SongsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  constructor(private dataSource: DataSource) {
+    console.log('dbName', dataSource.driver.database);
+  }
+
   configure(consumer: MiddlewareConsumer) {
     // consumer.apply(LoggerMiddleware).forRoutes('songs'); // op 1
     // consumer.apply(LoggerMiddleware).forRoutes({path: 'songs', method: RequestMethod.POST}); // op 2
